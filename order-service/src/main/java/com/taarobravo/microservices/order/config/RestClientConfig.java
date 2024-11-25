af72d940e78c581ce3ae79bd94d06306a76dbeea
Lautaro Bravo <lautabdls@gmail.com>
2024-11-24 21:49:59 -0300

    removing hardcoding urls

--- order-service/src/main/java/com/taarobravo/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/taarobravo/microservices/order/config/RestClientConfig.java
@@ -19,7 +19,7 @@ import java.time.Duration;
 @RequiredArgsConstructor
 public class RestClientConfig {
 
-    @Value("${inventory.url}")
+    @Value("${inventory.service.url}")
     private String inventoryUrl;
     private final ObservationRegistry observationRegistry;
 
